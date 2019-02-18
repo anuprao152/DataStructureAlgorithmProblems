66da703c0a624a0e32d9ae1e903c149c32d1c8b1
anuprao152 <anuprao85@gmail.com>
2019-02-18 17:16:42 -0500

    detect cycle

---
+++ MyWebPageApp/Programs/Programs/Graph/DetectCycleDAG.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programs
+{
+    //https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
+    public class DetectCycleDAG
+    {
+        public bool IsCycle(Graph graph)
+        {
+
+            bool[] visited = new bool[graph.Verticies.Count];
+            bool[] recursionArr = new bool[graph.Verticies.Count];
+
+            for(int i=0; i< graph.Verticies.Count; i++)
+            {
+                if (IsCycleUtil(graph.Verticies[i], visited, recursionArr))
+                    return true;
+            }
+
+            return false;
+            
+        }
+
+        public bool IsCycleUtil(Vertex v, bool[] visited, bool[] recursionArr)
+        {
+            if(recursionArr[v.vId])
+                return true;
+
+            if(visited[v.vId])
+                return false;
+
+            visited[v.vId] = true;
+            recursionArr[v.vId] = true;
+
+            for(int i=0;i < v.Neighbours.Count; i++)
+            {
+                var neighbhour = v.Neighbours[i];
+
+                if (IsCycleUtil(neighbhour, visited, recursionArr))
+                    return true;
+            }
+
+            recursionArr[v.vId] = false;
+
+            return false;
+        }
+
+    }
+}
---
+++ MyWebPageApp/Programs/Programs/Graph/Graph.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programs
+{
+    public class Graph
+    {
+        public List<Vertex> Verticies;
+
+        public Graph()
+        {
+            Verticies = new List<Vertex>();
+        }
+
+        public void AddEdge(Vertex source, Vertex dest)
+        {
+            source.Neighbours.Add(dest);
+        }
+    }
+
+    public class Vertex
+    {
+        public List<Vertex> Neighbours;
+        public int value;
+        public int vId;
+
+        public Vertex(int value)
+        {
+            this.value = value;
+            Neighbours = new List<Vertex>();
+        }
+    }
+}
