0c10d434d98e2b37431e6340659d1816889520f5
anuprao152 <anuprao85@gmail.com>
2019-04-03 21:40:04 -0400

    FB questions

---
+++ MyWebPageApp/Programs/Programs/Facebook/ArithmeticSlices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programs.Facebook
+{
+    //https://leetcode.com/articles/arithmetic-slices/
+    class ArithmeticSlices
+    {
+    }
+}
---
+++ MyWebPageApp/Programs/Programs/Facebook/ValidNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programs.Facebook
+{
+    //https://www.geeksforgeeks.org/check-given-string-valid-number-integer-floating-point/
+    class ValidNumber
+    {
+    }
+}
