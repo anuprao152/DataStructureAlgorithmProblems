e8a578db43341bea3c6700d3e6a7135dad5bbb08
anuprao152 <anuprao85@gmail.com>
2019-04-20 18:12:53 -0400

    smallest string containing all chars and evaluate expression to target

--- MyWebPageApp/Programs/Programs/Facebook/SmallestStringContainingAllChars.cs
+++ MyWebPageApp/Programs/Programs/Facebook/SmallestStringContainingAllChars.cs
@@ -9,5 +9,60 @@ namespace Programs.Facebook
     public class SmallestStringContainingAllChars
     {
         //https://www.geeksforgeeks.org/find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/
+
+        public string SmallestWindowContainingAllCharsFun(string str, string pat)
+        {
+            if (str == null || str.Length < pat.Length)
+            {
+                return string.Empty;
+            }
+
+            var pat_freq = new int[256];
+            var str_freq = new int[256];
+
+            for (int i = 0; i < pat.Length; i++)
+            {
+                pat_freq[pat[i]]++;
+            }
+
+            var count = 0;
+            var start = 0;
+            var minWindowLen = int.MaxValue;
+            string res = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                str_freq[str[i]]++;
+
+                if (pat_freq[str[i]] > 0 && str_freq[str[i]] <= pat_freq[str[i]])
+                    count++;
+
+                //window found that contains pattern
+                if (count == pat.Length)
+                {
+                    // try to minimize window
+                    // 1. if current char contain more frequency than pattern 
+                    // 2. cur char doesn't exist in pattern.
+                    while (str_freq[str[start]] > pat_freq[str[start]] || pat_freq[str[start]] == 0)
+                    {
+                        str_freq[str[start]]--;
+                        start++;
+                    }
+
+                    var curWindowLen = i - start + 1;
+
+                    if (curWindowLen < minWindowLen)
+                    {
+                        minWindowLen = curWindowLen;
+                        res = str.Substring(start, minWindowLen);
+                    }
+                }
+            }
+
+            if (start == 0 && minWindowLen == int.MaxValue)
+                return string.Empty;
+
+            return res;
+        }
     }
 }
---
+++ MyWebPageApp/Programs/Programs/Google/EvaluateExpressionTarget.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programs.Google
+{
+    public class EvaluateExpressionTarget
+    {
+        public void EvaluateExpressionTargetFun(int[] digit, int target)
+        {
+            DFSCore(digit, 0, 0, target, new List<string>(), "", 0);
+        }
+
+        public void DFSCore(int[] digit, int cur, int prev, int target, List<string> output, string expression, int ans)
+        {
+            if (cur == digit.Length)
+            {
+                if (ans == target)
+                {
+                    Print(output);
+                }
+            }
+
+            for (int i = cur; i < digit.Length; i++)
+            {
+                //first time
+                if (string.IsNullOrEmpty(expression))
+                {
+                    DFSCore(digit, cur + 1, cur, target, output, expression + digit[i].ToString(), digit[i]);
+                }
+                else
+                {
+                    //3 operations
+                    DFSCore(digit, cur + 1, cur, target, output, expression + "+" + digit[i], ans + digit[i]); // +
+                    DFSCore(digit, cur + 1, -cur, target, output, expression + "-" + digit[i], ans - digit[i]); // -
+                    DFSCore(digit, cur + 1, cur, target, output, expression + "*" + digit[i], ans - prev + prev * digit[i]); // *
+
+                }
+            }
+
+        }
+
+        public void Print(List<string> output)
+        {
+            for (int i = 0; i < output.Count; i++)
+            {
+                Console.WriteLine(output[i]);
+            }
+        }
+    }
+}
